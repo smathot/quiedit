d6c943cccd57d9f57da3653b301a3e6795c6d1a3
Sebastiaan Mathot <s.mathot@cogsci.nl>
2012-12-22 12:25:32 +0100

    Apply theme to export component

--- libquiedit/export.py
+++ libquiedit/export.py
@@ -54,6 +54,17 @@ class export(quiframe.quiframe):
 
 		self.setLayout(self.form)
 
+	def set_theme(self):
+
+		"""Set the theme"""
+
+
+		super(export, self).set_theme()
+		self.button_resume.setFont(self.quiedit.theme_font())
+		self.button_export_markdown.setFont(self.quiedit.theme_font())
+		self.button_export_txt.setFont(self.quiedit.theme_font())
+		self.button_export_simple_html.setFont(self.quiedit.theme_font())
+
 	def resume(self):
 
 		"""Resume editing"""
